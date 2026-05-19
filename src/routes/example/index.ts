eee32e671a930d5bbf8db6d73102da04e25cde67
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-05-19 12:27:33 -0400

    prettier

--- src/routes/example/index.ts
+++ src/routes/example/index.ts
@@ -1,9 +1,9 @@
-import { type FastifyPluginAsync } from 'fastify'
+import { type FastifyPluginAsync } from "fastify";
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return 'this is an example'
-  })
-}
+    fastify.get("/", async function (request, reply) {
+        return "this is an example";
+    });
+};
 
-export default example
+export default example;
